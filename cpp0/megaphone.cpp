f4272351e258494358ff60e2ed5316ca8aa8f56e
cyren4 <Cyren4@users.noreply.github.com>
2021-11-28 23:22:42 +0100

    cpp00 ex00 fini

---
+++ cpp0/megaphone.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <locale>
+
+char    *ft_toupper(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        if (isalpha(str[i]))
+            str[i] = toupper(str[i]);
+        i++;
+    }
+    return (str);
+}
+
+int main(int ac, char **av)
+{
+    int i = 1;
+
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"; 
+    while (i < ac)
+    {
+        std::cout << ft_toupper(av[i]) ;
+        i++;
+    }
+    std::cout << std::endl;
+}
