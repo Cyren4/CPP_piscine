61e4f6098a39880dd78091fcdea1214105638445
cyren4 <Cyren4@users.noreply.github.com>
2022-01-24 00:43:43 +0100

    cpp08 ex02 ongoing

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -1,12 +1,30 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
+#include <stack>
+#include <list>
+
 template<typename T>
-
-class MutantStack{
-    private:
+class MutantStack : public std::stack<T>{
     public:
+        MutantStack(){}
+        MutantStack(MutantStack const & src){
+            *this = src;
+        }
+        ~MutantStack(){}
 
+        MutantStack&    operator=(MutantStack const & src){
+            (void);
+            return src;
+        }
+
+        T* begin(){
+            return this->top();
+        }
+        T* end(){
+            return this->;
+        } 
 };
 
 #endif
